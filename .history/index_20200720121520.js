097c432a0d60dee7748808ab1b46f1c098de0f41
PBillingsby <philbillingsby@gmail.com>
2020-07-20 12:16:00 -0400

    Done.

--- .history/index_20200720121520.js
+++ .history/index_20200720121520.js
@@ -14,16 +14,12 @@ class Deciduous extends Tree {
     this.name = name 
   }
   static definition() {
-    return super.definition() + " Deciduous trees shed their leaves annually."
+    return super.definition() + "Deciduous trees shed their leaves annually."
   }
 }
 
 class Evergreen extends Tree {
   constructor(species, name) {
     super(species)
-    this.name = name
-  }
-  static definition() {
-    return super.definition() + " Evergreens keep their leaves all year round."
   }
 }
--- .history/index_20200720121526.js
+++ .history/index_20200720121526.js
@@ -14,7 +14,7 @@ class Deciduous extends Tree {
     this.name = name 
   }
   static definition() {
-    return super.definition() + " Deciduous trees shed their leaves annually."
+    return super.definition() + "Deciduous trees shed their leaves annually."
   }
 }
 
@@ -22,8 +22,5 @@ class Evergreen extends Tree {
   constructor(species, name) {
     super(species)
     this.name = name
-  }
-  static definition() {
-    return super.definition() + " Evergreens keep their leaves all year round."
   }
 }
